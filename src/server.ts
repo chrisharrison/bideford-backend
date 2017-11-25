52d60524d1ac53ed19f2637dac7d68123a3b7237
Chris Harrison <chris.harrison@funeralzone.co.uk>
2017-11-25 14:59:26 +0000

    Started Redis implementation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 import * as http from 'http';
 import * as WebSocket from 'ws';
+import * as redis from 'redis';
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+const redisClient = redis.createClient();
 
 let totalCount = 0;
 
@@ -17,6 +19,10 @@ wss.on('connection', (ws: WebSocket) => {
     });
 
 });
+
+setInterval(() => {
+    redisClient.zadd('', totalCount);
+}, 1000);
 
 //start our server
 server.listen(process.env.PORT || 8999, () => {
