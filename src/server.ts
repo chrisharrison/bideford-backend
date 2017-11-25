4dbb285c2f3e21c707de4b88b9dbe0f6e27c14b1
Sangheethan Subramaniam <ss@funeralzone.co.uk>
2017-11-25 14:58:22 +0000

    Add on close method

--- src/server.ts
+++ src/server.ts
@@ -6,17 +6,32 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+let closeTimeout = 2 * 1000; // 2 seconds
 let totalCount = 0;
 
+
 wss.on('connection', (ws: WebSocket) => {
+    add();
+    update();
+    ws.on('close', (event: Event) => {
+        remove();
+        update();
+    });
+});
 
+function add() {
     totalCount++;
+}
 
+function remove() {
+    totalCount--;
+}
+
+function update() {
     wss.clients.forEach(client => {
         client.send(`{"totalCount": ${totalCount}}`);
     });
-
-});
+}
 
 //start our server
 server.listen(process.env.PORT || 8999, () => {
