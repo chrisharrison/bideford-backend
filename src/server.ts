af04737fac4c69e5ef9580d360888005be7cc342
Chris Harrison <c.harrison1988@gmail.com>
2017-11-27 09:01:54 +0000

    Remove Redis

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import * as express from 'express';
 import * as http from 'http';
 import * as WebSocket from 'ws';
-// import * as redis from 'redis';
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-// const redisClient = redis.createClient();
 
 let totalCount = 0;
 
@@ -23,10 +21,6 @@ function update(value: number) {
         client.send(`{"totalCount": ${totalCount}}`);
     });
 }
-
-// setInterval(() => {
-//     redisClient.zadd('', totalCount);
-// }, 1000);
 
 //start our server
 server.listen(process.env.PORT || 8999, () => {
